25406095b6bfc521ea0a70028e368545b44e3119
Ian Eddy <ianmseddy@gmail.com>
2020-09-18 18:54:21 +0000

    fix typo

--- sourceClimateData.R
+++ sourceClimateData.R
@@ -37,74 +37,74 @@ sourceClimData <- function(scenario, model = 'CCSM4'){
                              fun = 'raster::stack') #get the high quality stuff
     }
 
-  }
-} else if (model == 'CanESM2') {
-  if (scenario == "RCP8.5") {
-    ATAstack <- prepInputs(url = "https://drive.google.com/file/d/17zBna_wegLmQs_m4FQd_JBhUcA0Da6lR/view?usp=sharing",
-                           targetFile = 'RIA_1ArcMinute_CanESM2_RCP85_ATA2011-2100.grd',
-                           alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP85_ATA2011-2100.gri',
-                           destinationPath = 'inputs',
-                           filename2 = 'inputs/RIA_1ArcMinute_CanESM2_85_ATA2011-2100.grd',
-                           fun = 'raster::stack')
-    CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/17zBna_wegLmQs_m4FQd_JBhUcA0Da6lR/view?usp=sharing',
-                           targetFile = 'RIA_1ArcMinute_CanESM2_RCP85_CMI2011-2100.grd',
-                           alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP85_CMI2011-2100.gri',
-                           destinationPath = 'inputs',
-                           filename2 = 'inputs/RIA_1ArcMinute_CanESM2_85_CMI2011-2100.grd',
-                           fun = 'raster::stack') #get the high quality stuff
+  } else if (model == 'CanESM2') {
+    if (scenario == "RCP8.5") {
+      ATAstack <- prepInputs(url = "https://drive.google.com/file/d/17zBna_wegLmQs_m4FQd_JBhUcA0Da6lR/view?usp=sharing",
+                             targetFile = 'RIA_1ArcMinute_CanESM2_RCP85_ATA2011-2100.grd',
+                             alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP85_ATA2011-2100.gri',
+                             destinationPath = 'inputs',
+                             filename2 = 'inputs/RIA_1ArcMinute_CanESM2_85_ATA2011-2100.grd',
+                             fun = 'raster::stack')
+      CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/17zBna_wegLmQs_m4FQd_JBhUcA0Da6lR/view?usp=sharing',
+                             targetFile = 'RIA_1ArcMinute_CanESM2_RCP85_CMI2011-2100.grd',
+                             alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP85_CMI2011-2100.gri',
+                             destinationPath = 'inputs',
+                             filename2 = 'inputs/RIA_1ArcMinute_CanESM2_85_CMI2011-2100.grd',
+                             fun = 'raster::stack') #get the high quality stuff
 
-  } else if (scenario == "RCP4.5") {
-    ATAstack <- prepInputs(url = "https://drive.google.com/file/d/1RMSiv4_M57IKDHrs9amMyctkyRWvclGH/view?usp=sharing",
-                           targetFile = 'RIA_1ArcMinute_CanESM2_RCP45_ATA2011-2100.grd',
-                           alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP45_ATA2011-2100.gri',
-                           destinationPath = 'inputs',
-                           filename2 = 'inputs/RIA_1ArcMinute_CanESM2_45_ATA2011-2100.grd',
-                           fun = 'raster::stack')
-    CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/1RMSiv4_M57IKDHrs9amMyctkyRWvclGH/view?usp=sharing',
-                           targetFile = 'RIA_1ArcMinute_CanESM2_RCP45_CMI2011-2100.grd',
-                           alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP45_CMI2011-2100.gri',
-                           destinationPath = 'inputs',
-                           filename2 = 'inputs/RIA_1ArcMinute_CanESM2_45_CMI2011-2100.grd',
-                           fun = 'raster::stack') #get the high quality stuff
+    } else if (scenario == "RCP4.5") {
+      ATAstack <- prepInputs(url = "https://drive.google.com/file/d/1RMSiv4_M57IKDHrs9amMyctkyRWvclGH/view?usp=sharing",
+                             targetFile = 'RIA_1ArcMinute_CanESM2_RCP45_ATA2011-2100.grd',
+                             alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP45_ATA2011-2100.gri',
+                             destinationPath = 'inputs',
+                             filename2 = 'inputs/RIA_1ArcMinute_CanESM2_45_ATA2011-2100.grd',
+                             fun = 'raster::stack')
+      CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/1RMSiv4_M57IKDHrs9amMyctkyRWvclGH/view?usp=sharing',
+                             targetFile = 'RIA_1ArcMinute_CanESM2_RCP45_CMI2011-2100.grd',
+                             alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP45_CMI2011-2100.gri',
+                             destinationPath = 'inputs',
+                             filename2 = 'inputs/RIA_1ArcMinute_CanESM2_45_CMI2011-2100.grd',
+                             fun = 'raster::stack') #get the high quality stuff
 
+    }
+  } else if (model == 'CNRM CM5') {
+    if (scenario == 'RCP4.5'){
+      ATAstack <- prepInputs(url = "https://drive.google.com/file/d/1KwobnIwRd9klNR4_45X_WOnjTLFQgS8J/view?usp=sharing",
+                             targetFile = 'RIA_1ArcMinute_CNRM_CM5_RCp45_ATA2011-2100.grd',
+                             alsoExtract = 'RIA_1ArcMinute_CNRM_CM5_RCp45_ATA2011-2100.gri',
+                             destinationPath = 'inputs',
+                             filename2 = 'inputs/RIA_1ArcMinute_CanESM2_45_ATA2011-2100.grd',
+                             fun = 'raster::stack')
+      CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/1KwobnIwRd9klNR4_45X_WOnjTLFQgS8J/view?usp=sharing',
+                             targetFile = 'RIA_1ArcMinute_CNRM_CM5_RCp45_CMI2011-2100.grd',
+                             alsoExtract = 'RIA_1ArcMinute_CNRM_CM5_RCp45_CMI2011-2100.gri',
+                             destinationPath = 'inputs',
+                             filename2 = 'inputs/RIA_1ArcMinute_CNRM_CM5_45_CMI2011-2100.grd',
+                             fun = 'raster::stack') #get the high quality stuff
+    } else {
+      stop("don't have RCP 8.5")
+    }
+  } else if (model == 'CSIRO MK3') {
+    if (scenario == 'RCP4.5') {
+      ATAstack <- prepInputs(url = "https://drive.google.com/file/d/1iye_G-F6Dxm7Pd4IDbgdwwvJ-KW9pj3-/view?usp=sharing",
+                             targetFile = 'RIA_1ArcMinute_CSIRO_mk3_ATA2011-2100.grd',
+                             alsoExtract = 'RIA_1ArcMinute_CSIRO_mk3_ATA2011-2100.gri',
+                             destinationPath = 'inputs',
+                             filename2 = 'inputs/RIA_1ArcMinute_CSIRO_mk3_45_ATA2011-2100.grd',
+                             fun = 'raster::stack')
+      CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/1iye_G-F6Dxm7Pd4IDbgdwwvJ-KW9pj3-/view?usp=sharing',
+                             targetFile = 'RIA_1ArcMinute_CSIRO_mk3_CMI2011-2100.grd',
+                             alsoExtract = 'RIA_1ArcMinute_CSIRO_mk3_CMI2011-2100.gri',
+                             destinationPath = 'inputs',
+                             filename2 = 'inputs/RIA_1ArcMinute_CSIRO_mk3_45_CMI2011-2100.grd',
+                             fun = 'raster::stack') #get the high quality stuff
+    } else {
+      stop("don't have RCP 8.5")
+    }
   }
-} else if (model == 'CNRM CM5') {
-  if (scenario == 'RCP4.5'){
-    ATAstack <- prepInputs(url = "https://drive.google.com/file/d/1KwobnIwRd9klNR4_45X_WOnjTLFQgS8J/view?usp=sharing",
-                           targetFile = 'RIA_1ArcMinute_CNRM_CM5_RCp45_ATA2011-2100.grd',
-                           alsoExtract = 'RIA_1ArcMinute_CNRM_CM5_RCp45_ATA2011-2100.gri',
-                           destinationPath = 'inputs',
-                           filename2 = 'inputs/RIA_1ArcMinute_CanESM2_45_ATA2011-2100.grd',
-                           fun = 'raster::stack')
-    CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/1KwobnIwRd9klNR4_45X_WOnjTLFQgS8J/view?usp=sharing',
-                           targetFile = 'RIA_1ArcMinute_CNRM_CM5_RCp45_CMI2011-2100.grd',
-                           alsoExtract = 'RIA_1ArcMinute_CNRM_CM5_RCp45_CMI2011-2100.gri',
-                           destinationPath = 'inputs',
-                           filename2 = 'inputs/RIA_1ArcMinute_CNRM_CM5_45_CMI2011-2100.grd',
-                           fun = 'raster::stack') #get the high quality stuff
-  } else {
-    stop("don't have RCP 8.5")
-  }
-} else if (model == 'CSIRO MK3') {
-  if (scenario == 'RCP4.5') {
-    ATAstack <- prepInputs(url = "https://drive.google.com/file/d/1iye_G-F6Dxm7Pd4IDbgdwwvJ-KW9pj3-/view?usp=sharing",
-                           targetFile = 'RIA_1ArcMinute_CSIRO_mk3_ATA2011-2100.grd',
-                           alsoExtract = 'RIA_1ArcMinute_CSIRO_mk3_ATA2011-2100.gri',
-                           destinationPath = 'inputs',
-                           filename2 = 'inputs/RIA_1ArcMinute_CSIRO_mk3_45_ATA2011-2100.grd',
-                           fun = 'raster::stack')
-    CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/1iye_G-F6Dxm7Pd4IDbgdwwvJ-KW9pj3-/view?usp=sharing',
-                           targetFile = 'RIA_1ArcMinute_CSIRO_mk3_CMI2011-2100.grd',
-                           alsoExtract = 'RIA_1ArcMinute_CSIRO_mk3_CMI2011-2100.gri',
-                           destinationPath = 'inputs',
-                           filename2 = 'inputs/RIA_1ArcMinute_CSIRO_mk3_45_CMI2011-2100.grd',
-                           fun = 'raster::stack') #get the high quality stuff
-  } else {
-    stop("don't have RCP 8.5")
-  }
+
+  climData = list(ATAstack = ATAstack,
+                  CMIstack = CMIstack,
+                  CMInormal = CMInormal)
+  return(climData)
 }
-
-climData = list(ATAstack = ATAstack,
-                CMIstack = CMIstack,
-                CMInormal = CMInormal)
-return(climData)
